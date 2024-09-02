ba401709a63b1f5ac4b330e081e01e04006eb98b
André Pedersen <andrped94@gmail.com>
2024-09-02 18:25:10 +0200

    Improved help for CLI

--- vsi2tif/vsi2tif.py
+++ vsi2tif/vsi2tif.py
@@ -6,11 +6,11 @@ def main():
     parser = ArgumentParser(description='vsi2tif - simple tool for converting images from cellSens VSI to Generic TIFF')
     parser.add_argument("-i", "--input", help="folder with input files", required=True)
     parser.add_argument("-o", "--output", help="folder for output files", required=True)
-    parser.add_argument("-b", "--bfconvert", help="path to bfconvert executable", required=True)
-    parser.add_argument("-c", "--compression", help="compression technique used for last conversion step", default="jpeg")
-    parser.add_argument("-p", "--plane", help="which image plane to convert image from - 0 is full resolution", default=0)
-    parser.add_argument("-s", "--tilesize", help="tile size to use during both conversion steps", default=1024)
-    parser.add_argument("-q", "--quality", help="compression quality used with JPEG compression", default=85)
+    parser.add_argument("-b", "--bfconvert", help="path to bfconvert tool", required=True)
+    parser.add_argument("-c", "--compression", help="compression technique used for last conversion step - default 'jpeg'", default="jpeg")
+    parser.add_argument("-p", "--plane", help="which image plane to convert image from - default 0", default=0)
+    parser.add_argument("-s", "--tilesize", help="tile size to use during both conversion steps - default 1024", default=1024)
+    parser.add_argument("-q", "--quality", help="compression quality used with JPEG compression - default 85", default=85)
     argv = parser.parse_args()
     
     if not os.path.isfile(argv.bfconvert):

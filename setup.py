28597f9eff2a80bc9c7dd2058792da757a124674
André Pedersen <andrped94@gmail.com>
2024-09-03 08:46:03 +0200

    Bump v0.1.1; support python>=3.6

--- setup.py
+++ setup.py
@@ -1,24 +1,24 @@
 from setuptools import setup, find_packages
 
 
-with open("README.md", "r", errors='ignore') as f:
+with open("README.md", "r", errors="ignore") as f:
     long_description = f.read()
 
 setup(
-    name='vsi2tif',
-    version='0.1.0',
+    name="vsi2tif",
+    version="0.1.1",
     author="André Pedersen, Sebastian Krossa, Erik Smistad",
     author_email="andrped94@gmail.com",
-    license='MIT',
+    license="MIT",
     description="A package for converting images from cellSens VSI format to tiled, pyramidal TIFF",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/andreped/vsi2tif",
     include_package_data=True,
-    packages=find_packages(exclude=('figures')), 
+    packages=find_packages(exclude=("figures")), 
     entry_points={
-        'console_scripts': [
-            'vsi2tif = vsi2tif.vsi2tif:main',
+        "console_scripts": [
+            "vsi2tif = vsi2tif.vsi2tif:main",
         ]
     },
     install_requires=["tqdm", "numpy"],
@@ -40,5 +40,5 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.8',
+    python_requires=">=3.6",
 )

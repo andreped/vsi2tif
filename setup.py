9918d77380830e1bf938c849654c78a702dc5d9c
André Pedersen <andrped94@gmail.com>
2024-09-11 10:46:07 +0200

    Bump v0.1.3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", errors="ignore") as f:
 
 setup(
     name="vsi2tif",
-    version="0.1.2",
+    version="0.1.3",
     author="André Pedersen, Sebastian Krossa, Erik Smistad",
     author_email="andrped94@gmail.com",
     license="MIT",
